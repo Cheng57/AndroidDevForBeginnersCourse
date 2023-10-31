b4c64db9edd6172d8fad22791df7e0bd79083de4
Cheng Wu <cwu57cn@gmail.com>
2023-10-31 06:17:37 -0500

    Fix code
    When it is the 5 guess and the right guess, also show congrats message instead of game over

--- GuessingGame/GuessingGame.java
+++ GuessingGame/GuessingGame.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 public class GuessingGame{
 	public static void main(String[] args){
-		System.out.println("Welcome!");
+		System.out.println("Welcome to GuessANum world!");
 
 		//only create a single 'Scanner' object and reuse it.
 		Scanner scanner = new Scanner(System.in);
@@ -25,34 +25,38 @@ public class GuessingGame{
 		if (yesOrNo.equals("y")){
 			Random rand = new Random();
 			int randNum = rand.nextInt(10) + 1;
+			//System.out.println("The random number is " + randNum);
 
 			int counter = 0;
+			int userNum = 0;
 			while (counter < 5){
-				System.out.println("Please enter a guessing number between 1 and 10:");
-				int userNum = scanner.nextInt();
+				counter++;
 
-				if (userNum > randNum && counter < 4){
+				System.out.println("Please enter a number between 1 and 10:");
+				userNum = scanner.nextInt();
+
+				if (userNum > randNum && counter < 5){
 					System.out.println("Guess a lower number");
 				}
 
-				if(userNum < randNum && counter < 4){
+				if(userNum < randNum && counter < 5){
 					System.out.println("Guess a higher number");
 				}
 
 				if (userNum == randNum){
-					System.out.println("Congrats! You got it.");
+					//System.out.println("Congrats! You got it in " + counter + " guess.");
 					break;
 				}
-
-				counter++;
 			}
 
-			if (counter == 5) {
+			if (counter <= 5 && userNum == randNum){
+				System.out.println("Congrats! You got it in " + counter + " guess.");
+			}
+			else{
 				System.out.println("Game Over");
-				System.out.println("The guessing number is " + randNum);
+				System.out.println("The number was " + randNum);
 			}
 		}
-
 		//close the Scanner when finished (optional but good practice).
 		scanner.close();
 	}
